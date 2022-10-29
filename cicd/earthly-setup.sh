2c030095214555c27934f115de5c73e39f2f6769
Adam Shamblin <adam@vexingworkshop.com>
2022-10-29 17:17:41 -0600

    update earthly version

--- cicd/earthly-setup.sh
+++ cicd/earthly-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget https://github.com/earthly/earthly/releases/download/v0.6.27/earthly-linux-amd64 \
+wget https://github.com/earthly/earthly/releases/download/v0.6.28/earthly-linux-amd64 \
   -O /usr/local/bin/earthly
 chmod +x /usr/local/bin/earthly
 /usr/local/bin/earthly bootstrap
